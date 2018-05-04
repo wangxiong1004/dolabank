2535e663077a2e0bef453e5284bd82c6eb463836
wangxiong <xiong.wang@dolabank.com>
2018-05-04 09:25:57 +0800

    build

--- src/App.vue
+++ src/App.vue
@@ -5,28 +5,6 @@
                 <router-view></router-view>
             </keep-alive>
         </div>
-        <!--<div class="bottom-nav border-1px" v-if="isNavShow">
-            <router-link :to="{name: 'home'}" class="dola-nav nav-home">
-                <i class="nav-img"></i>
-                <p class="nav-text">首页</p>
-            </router-link>
-            <router-link :to="{name: 'product'}" class="dola-nav nav-product">
-                <i class="nav-img"></i>
-                <p class="nav-text">投资</p>
-            </router-link>
-            <router-link :to="{name: 'user'}" class="dola-nav nav-user" v-if="isLogin">
-                <i class="nav-img"></i>
-                <p class="nav-text">我的</p>
-            </router-link>
-            <router-link :to="{name: 'login'}" class="dola-nav nav-user" v-else>
-                <i class="nav-img"></i>
-                <p class="nav-text">我的</p>
-            </router-link>
-            <router-link :to="{name: 'more'}" class="dola-nav nav-more">
-                <i class="nav-img"></i>
-                <p class="nav-text">更多</p>
-            </router-link>
-        </div>-->
     </div>
 </template>
 
@@ -35,8 +13,6 @@
         name: 'app',
         data() {
             return {
-                isNavShow: true,
-                isLogin: false
             };
         },
         watch: {
